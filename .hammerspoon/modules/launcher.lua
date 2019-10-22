1b551fe325f71ab3f54e816548903896d78f5396
muyang <mu.yang@shopee.com>
2019-10-22 15:59:35 +0800

    hammerspoon toggle 功能实现

--- .hammerspoon/modules/launcher.lua
+++ .hammerspoon/modules/launcher.lua
@@ -9,31 +9,32 @@ grid.setMargins({0, 0})
 
 applist = {
     {shortcut = 'I',appname = 'IntelliJ IDEA'},
-    {shortcut = 'C',appname = 'Google Chrome'},
-    {shortcut = 'T',appname = 'iTerm'},
+    {shortcut = 'H',appname = 'Google Chrome'},
     {shortcut = 'D',appname = 'Finder'},
     {shortcut = 'V',appname = 'Visual Studio Code'},
-    {shortcut = 'G',appname = 'GoLand'}
+    {shortcut = 'G',appname = 'GoLand'},
+    {shortcut = 'T',appname = 'iTerm'},
+    {shortcut = 'M',appname = 'Mattermost'}
     --{shortcut = 'Y',appname = 'Activity Monitor'},
     --{shortcut = 'P',appname = 'System Preferences'},
 }
 
 fnutils.each(applist, function(entry)
     hotkey.bind({'ctrl', 'alt'}, entry.shortcut, entry.appname, function()
-        application.launchOrFocus(entry.appname)
-        toggle_application(applist[i].appname)
+        toggle_application(entry.appname)
     end)
 end)
 
 -- Toggle an application between being the frontmost app, and being hidden
 function toggle_application(_app)
     local app = appfinder.appFromName(_app)
-    if not app then
+    print("app:", app)
+    if (not app) then
         application.launchOrFocus(_app)
         return
     end
     local mainwin = app:mainWindow()
-    if mainwin then
+    if (mainwin) then
         if mainwin == window.focusedWindow() then
             mainwin:application():hide()
         else
